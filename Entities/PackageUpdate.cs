dcea592b914b91970bb8164ac39c2009b4833f70
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-05-11 10:28:55 -0300

    Updates

--- Entities/PackageUpdate.cs
+++ Entities/PackageUpdate.cs
@@ -2,18 +2,17 @@ namespace DevTrackR.API.Entities
 {
     public class PackageUpdate
     {
-        public PackageUpdate(String Status)
+        public PackageUpdate(String status, int packageId)
         {
+            PackageId = packageId;
+            Status = status;
+            UpdateDate = DateTime.Now;
+            
 
         }
-        public PackageUpdate(int id, String status, DateTime updateDate)
-        {
-            this.Id = id;
-            this.Status = status;
-            this.UpdateDate = updateDate;
-
-        }
+        
         public int Id { get; private set; }
+        public int PackageId { get; private set; }
         public String Status { get; private set; }
         public DateTime UpdateDate { get; private set; }
     }
--- Entities/package.cs
+++ Entities/package.cs
@@ -1,28 +1,22 @@
-using System.Reflection.PortableExecutable;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace DevTrackR.API.Entities;
 
 public class package
 {
     public package(int title, decimal weight)
     {
-        
+        Code = Guid.NewGuid().ToString();
         Title = title;
         Weight = weight;
+        Delivered = false;
+        PostedAt = DateTime.Now;
+        Updates = new List<PackageUpdate>();
     }
 
     public int Id { get; private set; }
-   public String Code { get; private set; } 
-   public int  Title { get; private set; }
-   public decimal Weight  { get; private set; }
-   public bool Delivered  { get; private set; }
-   public DateTime PostedAt { get; private set; }
-   public int MyProperty { get; private set; }
-
-   public List<PackageUpdate> Updates { get; private set; }
-
+    public String Code { get; private set; } 
+    public int  Title { get; private set; }
+    public decimal Weight  { get; private set; }
+    public bool Delivered  { get; private set; }
+    public DateTime PostedAt { get; private set; }
+    public List<PackageUpdate> Updates { get; private set; }
 }
